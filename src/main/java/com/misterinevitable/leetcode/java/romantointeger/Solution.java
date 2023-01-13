badb6b9b3ac17c522f488c0c96d29c790fbf0774
aldumas <adumas+dev@gmail.com>
2023-01-13 14:01:45 -0700

    Replaced a map with a switch

--- src/main/java/com/misterinevitable/leetcode/java/romantointeger/Solution.java
+++ src/main/java/com/misterinevitable/leetcode/java/romantointeger/Solution.java
@@ -1,32 +1,33 @@
 package com.misterinevitable.leetcode.java.romantointeger;
 
-import java.util.Map;
-
 public class Solution {
-    private static final Map<Character, Integer> digits = Map.of(
-            'I', 1,
-            'V', 5,
-            'X', 10,
-            'L', 50,
-            'C', 100,
-            'D', 500,
-            'M', 1000
-    );
-
     public static int romanToInt(String s) {
         int value = 0;
         for (int i = 0; i < s.length(); ++i) {
             if (isCompositeDigit(s, i)) {
-                value += digits.get(s.charAt(i + 1)) - digits.get(s.charAt(i));
+                value += digit(s.charAt(i + 1)) - digit(s.charAt(i));
                 ++i; // Move past the 2nd character in the composite digit
             } else {
-                value += digits.get(s.charAt(i));
+                value += digit(s.charAt(i));
             }
         }
         return value;
     }
 
     private static boolean isCompositeDigit(String s, int i) {
-        return i < s.length() - 1 && digits.get(s.charAt(i)) < digits.get(s.charAt(i + 1));
+        return i < s.length() - 1 && digit(s.charAt(i)) < digit(s.charAt(i + 1));
+    }
+
+    private static int digit(char ch) {
+        return switch (ch) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X'-> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> throw new IllegalArgumentException("Not a Roman numeral: " + ch);
+        };
     }
 }
